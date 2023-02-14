a3143724b2bfdfc66a75ae18e0274dff04a1b43a
john-ciq <87781711+john-ciq@users.noreply.github.com>
2023-02-14 18:39:04 -0500

    Persist a derrived channel for peers

--- src/launch-group-communicator/index.ts
+++ src/launch-group-communicator/index.ts
@@ -3,9 +3,10 @@
 import { FDC3, types } from "@finsemble/finsemble-core";
 
 
+// The context type for launched peers to communicate on
 const CONTEXT_TYPE_LAUNCH_GROUP = "x-launch-group";
-// TODO: [JRG] Code for persisting channel name
-// const COMPONENT_STATE_FIELD_CHANNEL_NAME = "x-launch-group-channel-name";
+// The field name in component storage for the channel name
+const COMPONENT_STATE_FIELD_CHANNEL_NAME = "x-launch-group-channel-name";
 
 
 // TODO: Implementation should be sure that this function is modified to process incoming messages
@@ -14,61 +15,61 @@ launchGroupBridge.onLaunchGroupMessage = (payload) => {
 	console.log("DEFAULT onLaunchGroupMessage implementation:", payload);
 }
 
-// TODO: [JRG] Code for persisting channel name
-// let usingStoredLaunchGroup = false;
-// const saveLaunchedGroupChannelName = async (launchedGroupChannelName) => {
-// 	if (!!usingStoredLaunchGroup) {
-// 		console.log("not persisting new channel name; using current persisted channel name");
-// 	} else {
-// 		console.log('saving component state', COMPONENT_STATE_FIELD_CHANNEL_NAME, launchedGroupChannelName);
-// 		await FSBL.Clients.WindowClient.setComponentState({ field: COMPONENT_STATE_FIELD_CHANNEL_NAME, value: launchedGroupChannelName });
-// 	}
-// }
-//
-// const createUniqueLaunchedGroupChannelName = (launchedGroupChannelName) => {
-// 	return `uiq-${launchedGroupChannelName}-${new Date().getTime()}-${Math.random()}`;
-// }
+// Flag to determine if the launch group being used is from component storage
+let usingStoredLaunchGroup = false;
 
+/**
+ * Saves the name of a channel to use for future component invocations.
+ *
+ * @param launchedGroupChannelName the name of the channel to save
+ */
+const saveLaunchedGroupChannelName = async (launchedGroupChannelName) => {
+	if (!!usingStoredLaunchGroup) {
+		// console.log("not persisting new channel name; using current persisted channel name");
+	} else {
+		// console.log('saving component state', COMPONENT_STATE_FIELD_CHANNEL_NAME, launchedGroupChannelName);
+		// console.log('persisting channel name for future use', launchedGroupChannelName);
+		await FSBL.Clients.WindowClient.setComponentState({ field: COMPONENT_STATE_FIELD_CHANNEL_NAME, value: launchedGroupChannelName });
+	}
+}
+
+/**
+ * Creates a unique channel name, seeded from the supplied channel name.
+ *
+ * @param launchedGroupChannelName the name of the channel
+ * @returns a unique variation of the channel
+ */
+const createUniqueLaunchedGroupChannelName = (launchedGroupChannelName) => {
+	return `uiq-${launchedGroupChannelName}-${new Date().getTime()}-${Math.random()}`;
+}
+
+/**
+ * Gets the name of the channel to listen to for this launched peer group. This might be the launch group id, or
+ * perhaps a persisted value in the component state (if this component has been relaunched, for example).
+ *
+ * @returns the channel to listen to for this launched peer group
+ */
 const getLaunchedGroupChannelName = async () => {
 	let launchGroupId = ((await finsembleWindow.getOptions())?.data as any)?.windowInterop?.launchGroupId;
 
 	if (!launchGroupId) {
-		// TODO: [JRG] Code for persisting channel name
-		// // Get the launch group channel name from component state
-		// console.log("there is no launch group id, checking component state");
-		// let { data: componentStateLaunchGroupId } = await FSBL.Clients.WindowClient.getComponentState({ field: COMPONENT_STATE_FIELD_CHANNEL_NAME });
-		// if (!componentStateLaunchGroupId) {
-		// 	console.log("there is no persisted launch group id, will not attach a listener");
-		// 	return null;
-		// } else {
-		// 	usingStoredLaunchGroup = true;
-		// 	launchGroupId = componentStateLaunchGroupId;
-		// }
-
-		return null; // TODO: Remove this line when uncommenting above^
+		// Get the launch group channel name from component state
+		// console.log("There is no launch group id, checking component state");
+		let { data: componentStateLaunchGroupId } = await FSBL.Clients.WindowClient.getComponentState({ field: COMPONENT_STATE_FIELD_CHANNEL_NAME });
+		if (!componentStateLaunchGroupId) {
+			// console.log("There is no persisted launch group id, will not attach a listener");
+			return null;
+		} else {
+			usingStoredLaunchGroup = true;
+			launchGroupId = componentStateLaunchGroupId;
+		}
 	}
 
 	// Return the channel name
-	return `fdc3channel-${launchGroupId}`;
+	return `${launchGroupId}`;
 }
 
 const main = async (launchGroupBridge: any) => {
-
-	// TODO: [JRG] Code for persisting channel name
-	// /**
-	//  * Send signal data to other components in the launch group. Signal data is used within this code
-	//  * in order to communicate/broker between components, often for setup or configuration.
-	//  *
-	//  * @param signalData the signal data to send to other components in the launch group
-	//  */
-	// const sendSignalToLaunchedGroup = (signalData) => {
-	// 	launchedGroupFdc3Channel.broadcast({
-	// 		type: CONTEXT_TYPE_LAUNCH_GROUP,
-	// 		id: {
-	// 			signal: JSON.stringify(signalData)
-	// 		}
-	// 	});
-	// }
 
 	/**
 	 * Send messages to other components in the launch group. Any component in the launch group
@@ -85,64 +86,48 @@ const main = async (launchGroupBridge: any) => {
 		});
 	}
 
-	// TODO: [JRG] Code for persisting channel name
-	// // Do not re-attach a listener if the launchGroupBridge is already listening
-	// if (!!launchGroupBridge.isListening) {
-	// 	console.log(`already listening to channel "${launchGroupBridge.launchedGroupFdc3ChannelName}"`);
-	// 	return;
-	// }
+
+	// TODO: Do not re-attach a listener if the launchGroupBridge is already listening
 
 
 	// Get the launched group channel name
 	const launchedGroupFdc3ChannelName = await getLaunchedGroupChannelName();
 	if (!launchedGroupFdc3ChannelName) {
-		console.log("there is no launch group id, will not attach a listener");
+		console.warn("There is no launch group id, will not attach a listener");
 		return;
 	}
 
 
-	// Join the launch group channel
+	// Get the launch group channel
 	const launchedGroupFdc3Channel = await fdc3.getOrCreateChannel(launchedGroupFdc3ChannelName);
-	// TODO: [JRG] Code for persisting channel name
-	// // Designate the launch group channel name
-	// launchGroupBridge.channelName = launchedGroupFdc3ChannelName;
-
-
+	//
+	// Add a listener
 	launchedGroupFdc3Channel.addContextListener(CONTEXT_TYPE_LAUNCH_GROUP, (context: any) => {
 		// FDC3 specifies that the "id" attribute may contain string/string mappings; use the "id" to keep in spec
 		const messageData = context.id;
 
-		// See if this FDC3 message has a signal (coordinating message for components in the launch group)
-		if (messageData.signal) {
-			const parsedSignal = JSON.parse(messageData.signal);
-
-			// TODO: [JRG] Code for persisting channel name
-			// // If the signal is a persisted channel name
-			// if (!!parsedSignal?.persistedChannelName) {
-			// 	// Then save that for the next incarnation of this particular component
-			// 	saveLaunchedGroupChannelName(parsedSignal.persistedChannelName);
-			// }
-		}
-
 		// If there is a payload, then delegate to the launch group message handler
 		if (messageData.payload) {
-			console.log('got payload', messageData.payload);
+			// console.log('got payload', messageData.payload);
 			launchGroupBridge?.onLaunchGroupMessage?.(JSON.parse(messageData.payload));
 		}
 	});
-	// Designate that this component is listening
-	// TODO: [JRG] Code for persisting channel name
-	// launchGroupBridge.isListening = true;
-	console.log(`listening on channel "${launchedGroupFdc3ChannelName}"`);
+	// console.log('listening to', launchedGroupFdc3Channel.id);
 
-	// TODO: [JRG] Code for persisting channel name
-	// // 1.) Create a unique channel name
-	// const persistedChannelName = createUniqueLaunchedGroupChannelName(launchedGroupFdc3ChannelName);
-	// // 2.) Save in component state
-	// await saveLaunchedGroupChannelName(persistedChannelName);
-	// // 3.) Broadcast on signal
-	// sendSignalToLaunchedGroup({ persistedChannelName });
-	// // 4.) Listen on signal for any updates <-- This is done in the addition of the context listener (but timing issues abound)
+	// Synch and persist the brokered channel name
+	if (!usingStoredLaunchGroup) {
+		// 1.) Create a unique channel name
+		const persistedChannelName = createUniqueLaunchedGroupChannelName(launchedGroupFdc3ChannelName);
+		// 2.) Save in component state
+		await saveLaunchedGroupChannelName(persistedChannelName);
+		// Add a listener to the distributed store; the listener will receive any updated future channel and persist that in the component state
+		let { data: store } = await FSBL.Clients.DistributedStoreClient.createGlobalStore({ store: launchedGroupFdc3ChannelName });
+		// 3.) Listen on signal for any updates <-- This is done in the addition of the context listener (but timing issues abound)
+		store?.addListener([launchedGroupFdc3ChannelName, 'channelName'], (err, res) => saveLaunchedGroupChannelName(res.value));
+		//
+		// 4.) Publish the new name to the distributed store
+		store?.set([launchedGroupFdc3ChannelName, 'channelName'], persistedChannelName);
+	}
 
 
 	// Append the "sendMessageToLaunchedGroup" to the launchGroupBridge object
